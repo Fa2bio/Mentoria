e2639db37aa3597c1a7d47d482a37e29f91bc2d0
Fábio <fabio.s0@hotmail.com>
2023-05-31 20:04:01 -0300

    Update project

--- Arquivos_aula/Trabalhos/Trabalho_Banco_de_dados/3.1 - Modelo_Fisico.sql
+++ Arquivos_aula/Trabalhos/Trabalho_Banco_de_dados/3.1 - Modelo_Fisico.sql
@@ -2,49 +2,54 @@ CREATE DATABASE trabalho_ads;
 USE trabalho_ads;
 
 create table fornecedor(
-	id BIGINT NOT NULL AUTO_INCREMENT,
+	codigo_fornecedor BIGINT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     info_contato VARCHAR(1000) NOT NULL,
     
-    PRIMARY KEY (id)
+    PRIMARY KEY (codigo_fornecedor)
 );
 
 create table produto(
-	id BIGINT NOT NULL AUTO_INCREMENT,
+	codigo_produto BIGINT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(1000) NOT NULL,
 	preco DECIMAL(10,2) NOT NULL,
     estoque_disponivel BIGINT NOT NULL,
     fornecedor_id BIGINT NOT NULL,
     
-    PRIMARY KEY (id),
-    CONSTRAINT fk_produto_fornecedor FOREIGN KEY (fornecedor_id) REFERENCES fornecedor (id)
+    PRIMARY KEY (codigo_produto),
+    CONSTRAINT fk_produto_fornecedor FOREIGN KEY (fornecedor_id) REFERENCES fornecedor (codigo_fornecedor)
 );
 
 create table cliente(
-	id BIGINT NOT NULL AUTO_INCREMENT,
+	codigo_cliente BIGINT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     endereco VARCHAR(1000) NOT NULL,
     telefone VARCHAR(16) NOT NULL,
     
-    PRIMARY KEY (id)
-);
-
-create table compras(
-    data_compra DATE NOT NULL,
-    produto_id BIGINT NOT NULL,
-    cliente_id BIGINT NOT NULL,
-    
-    CONSTRAINT fk_compras_produto FOREIGN KEY (produto_id) REFERENCES produto (id),
-    CONSTRAINT fk_compras_cliente FOREIGN KEY (cliente_id) REFERENCES cliente (id)
+    PRIMARY KEY (codigo_cliente)
 );
 
 create table servico(
-	id BIGINT NOT NULL AUTO_INCREMENT,
+	codigo_servico BIGINT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     duracao TIME NOT NULL,
 	disponibilidade TINYINT,
     
-    PRIMARY KEY (id)
+    PRIMARY KEY (codigo_servico)
 );
+
+create table compras(
+	codigo_compra BIGINT NOT NULL AUTO_INCREMENT,
+    data_compra DATE NOT NULL,
+    produto_codigo BIGINT,
+    cliente_codigo BIGINT NOT NULL,
+    servico_codigo BIGINT,
+    
+    PRIMARY KEY (codigo_compra),
+    CONSTRAINT fk_compras_produto FOREIGN KEY (produto_codigo) REFERENCES produto (codigo_produto),
+    CONSTRAINT fk_compras_cliente FOREIGN KEY (cliente_codigo) REFERENCES cliente (codigo_cliente),
+    CONSTRAINT fk_compras_servico FOREIGN KEY (servico_codigo) REFERENCES servico (codigo_servico)
+);
+
--- Arquivos_aula/Trabalhos/Trabalho_Banco_de_dados/3.2 - Modelo_Fisico_insert.sql
+++ Arquivos_aula/Trabalhos/Trabalho_Banco_de_dados/3.2 - Modelo_Fisico_insert.sql
@@ -7,9 +7,13 @@ insert into produto values (2, "Shampoo Para Cães", "Anti-pulga", 18.99, 15,2);
 insert into cliente values (1, "Rosa Maria", "Rua Augusta, 39, São Paulo, São Paulo", "552191313-1122");
 insert into cliente values (2, "Paula Carvalho", "Rua Aleatoria, 40, Santos, São Paulo", "552190231-1057");
 
-insert into compras values ("2023-01-05", 1,1);
-insert into compras values ("2023-02-10", 2,1);
-insert into compras values ("2022-12-08", 2,2);
-
 insert into servico values (1,"Tosa", 39.90, "00:10:00.00", true);
 insert into servico values (2,"Banho", 29.90, "00:15:00.00", true);
+
+insert into compras values (1,"2023-01-05", 1,1,null);
+insert into compras values (2,"2023-02-10", 2,1,null);
+insert into compras values (3,"2022-12-08", 2,2,null);
+insert into compras values (4,"2022-12-08", null,2,1);
+insert into compras values (5,"2022-12-08", null,1,2);
+
+select * from compras;
