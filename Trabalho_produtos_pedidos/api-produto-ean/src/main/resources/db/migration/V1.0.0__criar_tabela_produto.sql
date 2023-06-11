91235fa63ea3f48373444ec53205a22756c35f4a
Fábio <fabio.s0@hotmail.com>
2023-06-11 12:09:15 -0300

    Update V1.0.0__criar_tabela_produto.sql

--- Trabalho_produtos_pedidos/api-produto-ean/src/main/resources/db/migration/V1.0.0__criar_tabela_produto.sql
+++ Trabalho_produtos_pedidos/api-produto-ean/src/main/resources/db/migration/V1.0.0__criar_tabela_produto.sql
@@ -2,6 +2,6 @@ CREATE TABLE produto (
 	codigo_produto BIGINT PRIMARY KEY IDENTITY,
 	nome VARCHAR(100) NOT NULL,
 	categoria VARCHAR(100) NOT NULL,
-	codigo_ean VARCHAR(13) NOT NULL,
+	codigo_ean VARCHAR(13) UNIQUE NOT NULL,
 	data_validade DATE NOT NULL
 );
