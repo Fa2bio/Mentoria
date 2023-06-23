c588c95aaecc5f4f61fa15849f3dd9173ed91b48
Fábio <fabio.s0@hotmail.com>
2023-06-23 02:03:17 -0300

    Create Multiplicar.java

---
+++ Lambdas/src/funcao/Multiplicar.java
@@ -0,0 +1,18 @@
+package funcao;
+
+import interfaces.Calcular;
+
+public class Multiplicar implements Calcular{
+
+	@Override
+	public double executar(double a, double b) {
+		return a*b;
+	}
+
+	@Override
+	public boolean comparar(double a, double b) {
+		if(a>b) return true;
+		return false;
+	}
+
+}
