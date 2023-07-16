d0f6eb4caa7f995ab86d3edcd265295ffc7a2d1e
Fábio <fabio.s0@hotmail.com>
2023-07-16 12:39:06 -0300

    Create Q30.java

---
+++ Exercicos/src/introducaoAoJava/q30/Q30.java
@@ -0,0 +1,15 @@
+package introducaoAoJava.q30;
+
+import java.util.Scanner;
+
+public class Q30 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada= new Scanner(System.in);
+		int a=1;
+		int b=2;
+		int c=3;
+		System.out.println(a+"-" +b+"-"+c);
+	    }
+}
