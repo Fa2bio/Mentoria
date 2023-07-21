a51407d32bcf8983f1a9b9536ffcee757195aec4
Fabio <fabio.s0@hotmail.com>
2023-07-21 19:37:15 -0300

    Create Vaga.java

---
+++ Exercicos/src/orientacaoobjetos/q9/Vaga.java
@@ -0,0 +1,35 @@
+package orientacaoobjetos.q9;
+
+public class Vaga {
+
+	private int numeroVaga;
+	private boolean ocupado;
+	private final Voo voo;	
+	
+	public Vaga(int numeroVaga, boolean ocupado, Voo voo) {
+		this.numeroVaga = numeroVaga;
+		this.ocupado = ocupado;
+		this.voo = voo;
+	}
+
+	public int getNumeroVaga() {
+		return numeroVaga;
+	}
+
+	public void setNumeroVaga(int numeroVaga) {
+		this.numeroVaga = numeroVaga;
+	}
+
+	public boolean isOcupado() {
+		return ocupado;
+	}
+
+	public void setOcupado(boolean ocupado) {
+		this.ocupado = ocupado;
+	}
+
+	public Voo getVoo() {
+		return voo;
+	}
+
+}
