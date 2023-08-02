ca9b387a3a195a2481037870d8b8192e929ffaed
Fábio <fabio.s0@hotmail.com>
2023-08-02 19:44:48 -0300

    Update Tabela_Produto_DQL.sql

--- Exercicio_Banco_de_dados/Tabela_Produto_DQL.sql
+++ Exercicio_Banco_de_dados/Tabela_Produto_DQL.sql
@@ -29,7 +29,38 @@ FROM produto as p
 GROUP BY p.id_nf
 ORDER BY valor_total DESC;
 
-SELECT p.cod_prod, MAX(p.quantidade) 
+SELECT p.cod_prod, MAX(p.quantidade) AS mais_vendidos
 FROM produto AS p 
 GROUP BY p.cod_prod
-ORDER BY valor_total DESC;
+ORDER BY mais_vendidos DESC;
+
+SELECT * FROM produto;
+
+SELECT p.id_nf, p.cod_prod
+FROM produto AS p
+WHERE p.quantidade > 10
+GROUP BY p.id_nf, p.cod_prod;
+
+SELECT p.id_nf,
+ROUND(SUM(p.quantidade * p.valor_unit),2) AS valor_total
+FROM produto AS p
+GROUP BY p.id_nf
+HAVING valor_total>200;
+
+SELECT p.cod_prod, 
+ROUND(AVG(p.desconto),2) AS media
+FROM produto AS p
+GROUP BY p.cod_prod;
+
+SELECT p.cod_prod,
+MIN(p.desconto) AS menor_desconto,
+MAX(p.desconto) AS maior_desconto,
+ROUND(AVG(p.desconto),2) AS media
+FROM produto AS p
+GROUP BY p.cod_prod;
+
+SELECT p.id_nf,
+COUNT(p.id_item) AS qtd_itens
+FROM produto AS p
+GROUP BY p.id_nf
+HAVING qtd_itens >= 3;
