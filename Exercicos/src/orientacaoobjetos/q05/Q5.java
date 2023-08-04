2d91b8445c6c706da41b0ffc317faf92f305053a
Fábio <fabio.s0@hotmail.com>
2023-08-04 14:43:14 -0300

    Create Q5.java

---
+++ Exercicos/src/orientacaoobjetos/q05/Q5.java
@@ -0,0 +1,11 @@
+package orientacaoobjetos.q05;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		
+		Alunos a = new Alunos("0123", "Fabio", 3.0, 5.0,1.0);
+		System.out.println(a.media());
+		System.out.println(a.mediafinal());
+	}
+}
