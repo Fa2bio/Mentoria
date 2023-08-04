93bd9130be53571bb2c54b5abbc2796f138fed0a
Fábio <fabio.s0@hotmail.com>
2023-08-04 14:44:56 -0300

    Update

--- Exercicos/src/orientacaoobjetos/q6/Q6.java
+++
@@ -1,9 +0,0 @@
-package orientacaoobjetos.q6;
-
-public class Q6 {
-
-	public static void main(String[] args) {
-		Invoice i = new Invoice("123","Produto de beleza",10,10.9);
-		System.out.println(i.getInvoiceAmount());
-	}
-}
