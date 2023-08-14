20c2f83c807eff9e9c299537ba5088257f089904
Fábio <fabio.s0@hotmail.com>
2023-08-13 21:46:51 -0300

    Create CalculadoraCientifica.java

---
+++ Exercicos/src/orientacaoobjetos/q13/CalculadoraCientifica.java
@@ -0,0 +1,12 @@
+package orientacaoobjetos.q13;
+
+public class CalculadoraCientifica extends Calculadora{
+
+	public double raizQuadrada(double n) {
+		return Math.sqrt(n);
+	}
+	
+	public double calcularPotencia(double a, double b) {
+		return Math.pow (a,b);
+	}
+}
