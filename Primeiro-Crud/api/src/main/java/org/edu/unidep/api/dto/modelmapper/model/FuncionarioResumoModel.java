c9b7840d67ff08feffb8eb4855ee90d5fbfeedd2
Fábio <fabio.s0@hotmail.com>
2023-08-24 16:03:31 -0300

    Create FuncionarioResumoModel.java

---
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/model/FuncionarioResumoModel.java
@@ -0,0 +1,14 @@
+package org.edu.unidep.api.dto.modelmapper.model;
+
+public class FuncionarioResumoModel {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
