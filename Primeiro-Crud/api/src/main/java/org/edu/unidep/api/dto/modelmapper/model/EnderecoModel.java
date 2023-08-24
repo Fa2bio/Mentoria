2e6e90bc433078dfd5e0678042ac26ecd7ca28b3
Fábio <fabio.s0@hotmail.com>
2023-08-24 16:03:29 -0300

    Create EnderecoModel.java

---
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/model/EnderecoModel.java
@@ -0,0 +1,55 @@
+package org.edu.unidep.api.dto.modelmapper.model;
+
+public class EnderecoModel {
+
+	private String cep;
+	
+	private String bairro;
+	
+	private String localidade;
+	
+	private String logradouro;
+
+	private String complemento;
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+}
