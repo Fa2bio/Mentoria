1d38c8288eab6f6b17c5fce6fb754c5c87539189
Fábio <fabio.s0@hotmail.com>
2023-08-26 11:19:43 -0300

    Update ClienteService.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/service/ClienteService.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/service/ClienteService.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 
 import org.edu.unidep.domain.exception.ClienteNaoEncontradoException;
 import org.edu.unidep.domain.model.Cliente;
+import org.edu.unidep.domain.model.Endereco;
 import org.edu.unidep.domain.repository.ClienteRepository;
 
 @ApplicationScoped
@@ -16,21 +17,27 @@ public class ClienteService {
 	@Inject
 	private ClienteRepository clienteRepository;
 	
+	@Inject
+	private EnderecoService enderecoService;
+	
 	public List<Cliente> listarTodosClientes(){
 		return clienteRepository.listar();
 	}
 	
 	@Transactional
-	public void salvarCliente(Cliente cliente) {
+	public void salvarCliente(Cliente cliente, String cep) {
+		Endereco endereco = enderecoService.enderecoViaCep(cep);
+		cliente.setEndereco(endereco);
 		clienteRepository.salvar(cliente);
 	}
 	
 	@Transactional
-	public void atualizarCliente(Long clienteId, Cliente clienteAtualizado) {
+	public void atualizarCliente(Long clienteId, Cliente clienteAtualizado, String cep) {
+		Endereco endereco = enderecoService.enderecoViaCep(cep);
 		Cliente clienteEncontrado = buscarOuFalhar(clienteId);
 		clienteEncontrado.setNome(clienteAtualizado.getNome());
 		clienteEncontrado.setCpf(clienteAtualizado.getCpf());
-		clienteEncontrado.setEndereco(clienteAtualizado.getEndereco());
+		clienteEncontrado.setEndereco(endereco);
 	}
 	
 	@Transactional
