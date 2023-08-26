bcf19314b011f7a0e6036ed534aaddab2082a8d1
Fábio <fabio.s0@hotmail.com>
2023-08-26 11:19:20 -0300

    Create CepInvalidoException.java

---
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/exception/CepInvalidoException.java
@@ -0,0 +1,11 @@
+package org.edu.unidep.domain.exception;
+
+public class CepInvalidoException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public CepInvalidoException(String message) {
+		super(message);
+	}
+
+}
