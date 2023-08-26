49502d06bbcd7a8978433cb730d700eeaddc19fe
Fábio <fabio.s0@hotmail.com>
2023-08-26 11:19:33 -0300

    Update VolumeInfo.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/model/VolumeInfo.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/model/VolumeInfo.java
@@ -25,6 +25,12 @@ public class VolumeInfo {
 	@Column(name = "qtdPags", nullable = false)
 	private Long qtdPags;
 	
+	@Column(name = "isbn_10",length = 10, nullable = false)
+	private String isbn10;
+	
+	@Column(name = "isbn_13",length = 13, nullable = false)
+	private String isbn13;
+	
 	@Column(name = "autores")
 	@ElementCollection(targetClass = String.class)
 	private List<String> autores = new ArrayList<>();
@@ -58,6 +64,18 @@ public class VolumeInfo {
 	}
 	public void setQtdPags(Long qtdPags) {
 		this.qtdPags = qtdPags;
+	}
+	public String getIsbn10() {
+		return isbn10;
+	}
+	public void setIsbn10(String isbn10) {
+		this.isbn10 = isbn10;
+	}
+	public String getIsbn13() {
+		return isbn13;
+	}
+	public void setIsbn13(String isbn13) {
+		this.isbn13 = isbn13;
 	}
 	public List<String> getAutores() {
 		return autores;
