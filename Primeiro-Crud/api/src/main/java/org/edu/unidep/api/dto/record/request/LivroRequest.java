221668a840b601bff54180509e001f2d06fc949f
Fábio <fabio.s0@hotmail.com>
2023-08-27 02:20:09 -0300

    Update LivroRequest.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/record/request/LivroRequest.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/record/request/LivroRequest.java
@@ -1,5 +1,9 @@
 package org.edu.unidep.api.dto.record.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.edu.unidep.api.dto.record.model.LivroModel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -7,6 +11,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record LivroRequest(
 		
 		@JsonProperty("isbn")
+		@NotNull
+		@NotBlank
+		@Size(min = 10, max = 13)
 		String isbn
 		) {
 
