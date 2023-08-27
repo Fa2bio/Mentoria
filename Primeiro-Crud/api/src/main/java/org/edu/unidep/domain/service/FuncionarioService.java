2c587da9de502e51015b5a15526eb30dc88c6234
Fábio <fabio.s0@hotmail.com>
2023-08-27 02:20:36 -0300

    Update FuncionarioService.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/service/FuncionarioService.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/service/FuncionarioService.java
@@ -1,12 +1,17 @@
 package org.edu.unidep.domain.service;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
 
 import org.edu.unidep.api.dto.record.model.FuncionarioModel;
+import org.edu.unidep.api.dto.record.request.FuncionarioRequest;
 import org.edu.unidep.domain.exception.FuncionarioNaoEncontradoException;
 import org.edu.unidep.domain.model.Endereco;
 import org.edu.unidep.domain.model.Funcionario;
@@ -20,6 +25,15 @@ public class FuncionarioService {
 	
 	@Inject
 	private EnderecoService enderecoService;
+	
+	@Inject
+	private Validator validator;
+	
+	public void validarFuncionarioRequest(FuncionarioRequest funcionarioRequest) {
+		Set<ConstraintViolation<FuncionarioRequest>> constraintViolations = validator.validate(funcionarioRequest);
+		if(constraintViolations.isEmpty()) return;
+		else throw new ConstraintViolationException(constraintViolations);
+	}
 	
 	public List<Funcionario> listarTodosFuncionarios(){
 		return funcionarioRepository.listar();
