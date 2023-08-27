a541d2ae5ba6bd6546ba19045dd700bdcbc0a6a1
Fábio <fabio.s0@hotmail.com>
2023-08-27 02:20:07 -0300

    Update FuncionarioRequest.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/record/request/FuncionarioRequest.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/record/request/FuncionarioRequest.java
@@ -1,18 +1,31 @@
 package org.edu.unidep.api.dto.record.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.edu.unidep.api.dto.record.model.FuncionarioModel;
+import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record FuncionarioRequest(
 		
 		@JsonProperty("nome")
+		@NotNull
+		@NotBlank
 		String nome,
 		
 		@JsonProperty("cpf")
+		@NotNull
+		@NotBlank
+		@CPF
 		String cpf,
 		
 		@JsonProperty("cep")
+		@NotNull
+		@NotBlank
+		@Size(min = 8, max = 9)
 		String cep
 		) {
 
