d9b8cc21b6ffa7146ae59cf5dee253d5801a345b
Fábio <fabio.s0@hotmail.com>
2023-08-28 19:31:52 -0300

    Create PedidoAssembler.java

---
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/assembler/PedidoAssembler.java
@@ -0,0 +1,29 @@
+package org.edu.unidep.api.dto.modelmapper.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.edu.unidep.api.dto.modelmapper.response.PedidoResponse;
+import org.edu.unidep.domain.model.Pedido;
+import org.modelmapper.ModelMapper;
+
+@ApplicationScoped
+public class PedidoAssembler {
+	
+	@Inject
+	private ModelMapper modelMapper;
+
+	public PedidoResponse toResponse(Pedido pedido) {
+		return modelMapper.map(pedido, PedidoResponse.class);
+	}
+	
+	public List<PedidoResponse> toCollectionResponse(List<Pedido> pedidos){
+		return pedidos.stream()
+				.map(pedido -> toResponse(pedido))
+				.collect(Collectors.toList());
+	}
+	
+}
