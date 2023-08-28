9d85c162dc21b88049f793ed35953cc5a4a4c1ea
Fábio <fabio.s0@hotmail.com>
2023-08-28 19:32:13 -0300

    Create LivroResumoResponse.java

---
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/response/LivroResumoResponse.java
@@ -0,0 +1,15 @@
+package org.edu.unidep.api.dto.modelmapper.response;
+
+public class LivroResumoResponse {
+
+	private String isbn;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+}
