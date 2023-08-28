16995244ab04da7e15bac61e26ef5d09f60178e4
Fábio <fabio.s0@hotmail.com>
2023-08-28 19:32:02 -0300

    Create ClienteResumoResponse.java

---
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/response/ClienteResumoResponse.java
@@ -0,0 +1,20 @@
+package org.edu.unidep.api.dto.modelmapper.response;
+
+public class ClienteResumoResponse {
+
+	private String nome;
+	private String cpf;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+}
