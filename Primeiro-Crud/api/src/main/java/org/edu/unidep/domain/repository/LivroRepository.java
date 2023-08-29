c8e72ca78fa7e687448e92ba598cbb4ed59702fb
Fábio <fabio.s0@hotmail.com>
2023-08-29 00:07:08 -0300

    Update LivroRepository.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/repository/LivroRepository.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/domain/repository/LivroRepository.java
@@ -8,6 +8,8 @@ import org.edu.unidep.domain.model.Livro;
 public interface LivroRepository {
 	List<Livro> listar();
 	Optional<Livro> buscarPorId(Long id);
+	Optional<Livro> buscarLivroPorIsbn10(String isbn);
+	Optional<Livro> buscarLivroPorIsbn13(String isbn);
 	void salvar(Livro livro);
 	void deletar(Livro livro);
 }
