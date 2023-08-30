de4b9331762cb5bf77cfd7f098db21271336b321
Fábio <fabio.s0@hotmail.com>
2023-08-30 14:11:03 -0300

    Update FuncionarioController.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/api/controller/FuncionarioController.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/controller/FuncionarioController.java
@@ -14,14 +14,23 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.edu.unidep.api.dto.record.assembler.FuncionarioAssembler;
 import org.edu.unidep.api.dto.record.model.FuncionarioModel;
 import org.edu.unidep.api.dto.record.request.FuncionarioRequest;
+import org.edu.unidep.api.exceptionhandler.ExceptionMessage;
 import org.edu.unidep.domain.model.Funcionario;
 import org.edu.unidep.domain.service.FuncionarioService;
 
 @Path("/funcionarios")
+@Tag(name = "Funcionario")
 public class FuncionarioController {
 
 	@Inject
@@ -34,6 +43,11 @@ public class FuncionarioController {
 	@GET
 	@Path("/listar")
 	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(description = "Lista Todos Os Funcionarios")
+	@APIResponses({
+		@APIResponse(responseCode = "200", description = "Ok"),
+		@APIResponse(responseCode = "204", description = "No Content")
+	})
 	public Response listarTodos() {
 		List<Funcionario> funcionarios = funcionarioService.listarTodosFuncionarios();
 		if(funcionarios.isEmpty()) return Response.status(Status.NO_CONTENT).build();
@@ -43,7 +57,16 @@ public class FuncionarioController {
 	@GET
 	@Path("/buscar/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response buscarFuncionario(@PathParam("id") Long id) {
+	@Operation(description = "Busca Funcionario Por Id")
+	@APIResponses({
+		@APIResponse(responseCode = "200", description = "Ok"),
+		@APIResponse(responseCode = "404", description = "Funcionario Não Encontrado", content = 
+				@Content(schema = @Schema(implementation = ExceptionMessage.class))
+		)			
+	})
+	public Response buscarFuncionario(
+			@Parameter(description = "Id do Funcionario", example = "1", required = true)
+			@PathParam("id") Long id) {
 		Funcionario funcionario = funcionarioService.buscarOuFalhar(id);
 		return Response.ok(funcionarioAssembler.toResponse(funcionario)).build();
 	}
@@ -52,9 +75,22 @@ public class FuncionarioController {
 	@PUT	
 	@Path("/atualizar/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response atualizarFuncionario(
+	@Operation(description = "Atualiza Funcionario Por Id")
+	@APIResponses({
+		@APIResponse(responseCode = "204", description = "Funcionario Atualizado"),
+		@APIResponse(responseCode = "400", description = "Requisição Nao Entendida", content = 
+		@Content(schema = @Schema(implementation = ExceptionMessage.class))),
+		@APIResponse(responseCode = "404", description = "Funcionario Não Encontrado", content = 
+				@Content(schema = @Schema(implementation = ExceptionMessage.class))
+		)			
+	})
+	public Response atualizarFuncionario(			
+			@Parameter(description = "Id do Funcionario", example = "1", required = true)
 			@PathParam("id") Long id,
-			@RequestBody FuncionarioRequest funcionarioAtualizado) {
+			
+			@RequestBody(description = "body", required = true)
+			FuncionarioRequest funcionarioAtualizado) {
+		
 		funcionarioService.validarFuncionarioRequest(funcionarioAtualizado);
 		FuncionarioModel funcionarioModel = FuncionarioRequest.toModel(funcionarioAtualizado);
 		funcionarioService.atualizarFuncionario(id, funcionarioModel);
@@ -65,7 +101,16 @@ public class FuncionarioController {
 	@POST
 	@Path("/registrar")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response registrar(@RequestBody FuncionarioRequest funcionario) {
+	@Operation(description = "Registra Um Funcionario")
+	@APIResponses({
+		@APIResponse(responseCode = "201", description = "Funcionario Registrado"),
+		@APIResponse(responseCode = "400", description = "Requisição Nao Entendida", content = 
+		@Content(schema = @Schema(implementation = ExceptionMessage.class)))			
+	})
+	public Response registrar(
+			@RequestBody(description = "body", required = true)
+			FuncionarioRequest funcionario) {
+		
 		funcionarioService.validarFuncionarioRequest(funcionario);
 		FuncionarioModel funcionarioModel = FuncionarioRequest.toModel(funcionario);
 		funcionarioService.salvarFuncionario(funcionarioModel);
@@ -76,7 +121,16 @@ public class FuncionarioController {
 	@DELETE
 	@Path("/excluir/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response excluir(@PathParam("id") Long id) {
+	@Operation(description = "Atualiza Funcionario Por Id")
+	@APIResponses({
+		@APIResponse(responseCode = "204", description = "Funcionario Deletado"),
+		@APIResponse(responseCode = "404", description = "Funcionario Não Encontrado", content = 
+				@Content(schema = @Schema(implementation = ExceptionMessage.class))
+		)			
+	})
+	public Response excluir(
+			@Parameter(description = "Id do Funcionario", example = "1", required = true)
+			@PathParam("id") Long id) {
 		funcionarioService.excluirFuncionario(id);
 		return Response.status(Status.NO_CONTENT).build();
 	}
