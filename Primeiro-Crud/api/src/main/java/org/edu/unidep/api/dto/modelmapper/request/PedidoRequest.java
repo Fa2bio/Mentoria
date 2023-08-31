5654c756d1ca6c77632a549c94873c86ec0cd24b
Fábio <fabio.s0@hotmail.com>
2023-08-31 10:34:33 -0300

    Update PedidoRequest.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/request/PedidoRequest.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/request/PedidoRequest.java
@@ -6,25 +6,31 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PedidoRequest {
 
-	@NotNull
+	@NotNull(message = "Data da Emissão Não Pode Ser Nula")
 	@JsonProperty("dataEmissao")
+	@Schema(description = "Data Da Emissão", example = "2023-08-30")
 	private LocalDate dataEmissao;
 	
-	@NotNull
+	@NotNull(message = "Id Do Cliente Não Pode Ser Nulo")
 	@JsonProperty("clienteId")
+	@Schema(description = "Id Do Cliente", example = "1")
 	private Long clienteId;
 	
-	@NotNull
+	@NotNull(message = "Id Do Funcionario Não Pode Ser Nulo")
 	@JsonProperty("funcionarioId")
+	@Schema(description = "Id Do Funcionario", example = "1")
 	private Long funcionarioId;
 	
-	@NotNull
+	@NotNull(message = "Os Itens Não Podem Ser Nulos")
 	@Valid
 	@JsonProperty("itens")
+	@Schema(description = "Lista De Itens")
 	private List<ItemRequest> itens;
 
 	public LocalDate getDataEmissao() {
