7f92f23075cc4f9cc4d0842489b3e159ab64098a
Fábio <fabio.s0@hotmail.com>
2023-08-31 10:34:35 -0300

    Update LivroRequest.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/record/request/LivroRequest.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/record/request/LivroRequest.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.edu.unidep.api.dto.record.model.LivroModel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -11,9 +12,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record LivroRequest(
 		
 		@JsonProperty("isbn")
-		@NotNull
-		@NotBlank
-		@Size(min = 10, max = 13)
+		@NotNull(message = "Isbn Do Livro Não Pode Ser Nulo")
+		@NotBlank(message = "Isbn Do Livro Não Pode Estar Em Branco")
+		@Size(min = 10, max = 13, message = "O ISBN Deve Possuir 10 ou 13 Caracteres")
+		@Schema(description = "Isbn Do Livro", example = "8535914846")
 		String isbn
 		) {
 
