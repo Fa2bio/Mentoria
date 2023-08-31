57d97b017fa5e503ed14f6049ac5825b6467a117
Fábio <fabio.s0@hotmail.com>
2023-08-31 10:34:30 -0300

    Update PedidoController.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/api/controller/PedidoController.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/controller/PedidoController.java
@@ -13,14 +13,23 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.edu.unidep.api.dto.modelmapper.assembler.PedidoAssembler;
 import org.edu.unidep.api.dto.modelmapper.request.PedidoRequest;
 import org.edu.unidep.api.dto.modelmapper.response.PedidoResponse;
+import org.edu.unidep.api.exceptionhandler.ExceptionMessage;
 import org.edu.unidep.domain.model.Pedido;
 import org.edu.unidep.domain.service.PedidoService;
 
 @Path("/pedidos")
+@Tag(name = "Pedido")
 public class PedidoController {
 
 	@Inject
@@ -32,6 +41,11 @@ public class PedidoController {
 	@GET
 	@Path("/listar")
 	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(description = "Listar Todos Os Pedidos")
+	@APIResponses({
+		@APIResponse(responseCode = "200", description = "Ok"),
+		@APIResponse(responseCode = "204", description = "No Content")
+	})
 	public Response listarTodos() {
 		List<Pedido> pedidos = pedidoService.listarTodosPedido();
 		if(pedidos.isEmpty()) return Response.status(Status.NO_CONTENT).build();
@@ -42,7 +56,16 @@ public class PedidoController {
 	@GET
 	@Path("/listarpeladadata/{data}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response listarPelaData(@PathParam("data") String data) {
+	@Operation(description = "Lista Pedidos Por Data")
+	@APIResponses({
+		@APIResponse(responseCode = "200", description = "Ok"),
+		@APIResponse(responseCode = "404", description = "Pedido Não Encontrado", content = 
+			@Content(schema = @Schema(implementation = ExceptionMessage.class)))
+	})
+	public Response listarPelaData(
+			@Parameter(description = "data", example = "2023-08-30", required = true)
+			@PathParam("data") String data) {
+		
 		LocalDate localdata = LocalDate.parse(data);
 		List<Pedido> pedidos = pedidoService.listarPelaData(localdata);
 		if(pedidos.isEmpty()) return Response.status(Status.NO_CONTENT).build();
@@ -53,7 +76,16 @@ public class PedidoController {
 	@GET
 	@Path("/listarpelonomecliente/{nome}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response listarPeloNomeCliente(@PathParam("nome") String nome) {
+	@Operation(description = "Lista Pedidos Por Nome Do Cliente")
+	@APIResponses({
+		@APIResponse(responseCode = "200", description = "Ok"),
+		@APIResponse(responseCode = "404", description = "Pedido Não Encontrado", content = 
+			@Content(schema = @Schema(implementation = ExceptionMessage.class)))
+	})
+	public Response listarPeloNomeCliente(
+			@Parameter(description = "nome", example = "Fábio", required = true)
+			@PathParam("nome") String nome) {
+		
 		List<Pedido> pedidos = pedidoService.listarPeloNomeCliente(nome);
 		if(pedidos.isEmpty()) return Response.status(Status.NO_CONTENT).build();
 		List<PedidoResponse> pedidosResponse = pedidoAssembler.toCollectionResponse(pedidos);
@@ -63,7 +95,16 @@ public class PedidoController {
 	@GET
 	@Path("/listarpelonomefuncionario/{nome}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response listarPeloNomeFuncionario(@PathParam("nome") String nome) {
+	@Operation(description = "Lista Pedidos Por Nome Do Funcionario")
+	@APIResponses({
+		@APIResponse(responseCode = "200", description = "Ok"),
+		@APIResponse(responseCode = "404", description = "Pedido Não Encontrado", content = 
+			@Content(schema = @Schema(implementation = ExceptionMessage.class)))
+	})
+	public Response listarPeloNomeFuncionario(
+			@Parameter(description = "nome", example = "Paulo", required = true)
+			@PathParam("nome") String nome) {
+		
 		List<Pedido> pedidos = pedidoService.listarPeloNomeFuncionario(nome);
 		if(pedidos.isEmpty()) return Response.status(Status.NO_CONTENT).build();
 		List<PedidoResponse> pedidosResponse = pedidoAssembler.toCollectionResponse(pedidos);
@@ -73,7 +114,15 @@ public class PedidoController {
 	@GET
 	@Path("/buscar/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response buscarPedido(@PathParam("id") Long id) {
+	@Operation(description = "Busca Pedidos Por Id")
+	@APIResponses({
+		@APIResponse(responseCode = "200", description = "Ok"),
+		@APIResponse(responseCode = "404", description = "Pedido Não Encontrado", content = 
+			@Content(schema = @Schema(implementation = ExceptionMessage.class)))
+	})
+	public Response buscarPedido(
+			@Parameter(description = "Id Do Pedido", example = "1", required = true)
+			@PathParam("id") Long id) {
 		Pedido pedido = pedidoService.buscarOuFalhar(id);
 		PedidoResponse pedidoResponse = pedidoAssembler.toResponse(pedido);
 		return Response.ok(pedidoResponse).build();
@@ -82,7 +131,15 @@ public class PedidoController {
 	@GET
 	@Path("/buscarpelocodigopedido/{codigo}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response buscarPedido(@PathParam("codigo") String codigo) {
+	@Operation(description = "Busca Pedidos Por Código")
+	@APIResponses({
+		@APIResponse(responseCode = "200", description = "Ok"),
+		@APIResponse(responseCode = "404", description = "Pedido Não Encontrado", content = 
+			@Content(schema = @Schema(implementation = ExceptionMessage.class)))
+	})
+	public Response buscarPedido(
+			@Parameter(description = "Código Do Pedido", example = "fe7d285e-3c62-4940-a1c1-411f2b1628c1", required = true)
+			@PathParam("codigo") String codigo) {
 		Pedido pedido = pedidoService.buscarPeloCodigo(codigo);
 		PedidoResponse pedidoResponse = pedidoAssembler.toResponse(pedido);
 		return Response.ok(pedidoResponse).build();
@@ -91,7 +148,16 @@ public class PedidoController {
 	@POST
 	@Path("/registrar")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response criar(@RequestBody PedidoRequest pedidoRequest) {
+	@Operation(description = "Busca Pedidos Por Código")
+	@APIResponses({
+		@APIResponse(responseCode = "201", description = "Pedido Registrado"),
+		@APIResponse(responseCode = "400", description = "Requisição Não Entendida", content = 
+			@Content(schema = @Schema(implementation = ExceptionMessage.class)))
+	})
+	public Response criar(			
+			@RequestBody(description = "body", required = true)
+			PedidoRequest pedidoRequest) {
+		
 		pedidoService.validarPedidoRequest(pedidoRequest);
 		pedidoService.salvarPedido(pedidoRequest);
 		return Response.status(Status.CREATED).build();
