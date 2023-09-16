5ecb447e7a883da3238b466a2cf3e728bcc51f3a
Fábio <fabio.s0@hotmail.com>
2023-09-16 16:27:01 -0300

    Update PedidoRepositoryImpl.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/infrastructure/impl/PedidoRepositoryImpl.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/infrastructure/impl/PedidoRepositoryImpl.java
@@ -1,12 +1,19 @@
 package org.edu.unidep.infrastructure.impl;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
+import org.edu.unidep.domain.filter.PedidoFilter;
 import org.edu.unidep.domain.model.Pedido;
 import org.edu.unidep.domain.repository.PedidoRepository;
 
@@ -87,6 +94,43 @@ PanacheRepository<Pedido>{
 		persist(pedido);
 	}
 
-
+	@Override
+	public List<Pedido> listarComFiltro(PedidoFilter pedidoFilter) {
+		CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Pedido> query = criteriaBuilder.createQuery(Pedido.class);
+		Root<Pedido> root = query.from(Pedido.class);
+		List<Predicate> predicates = new ArrayList<>();
+		
+		if(Pedido.class.equals(query.getResultType())) {
+			root.fetch("cliente");
+			root.fetch("funcionario");
+		}
+		
+		if(pedidoFilter.getDataPedidoInicio() != null) {
+			predicates.add(criteriaBuilder
+					.greaterThanOrEqualTo(root.get("dataEmissao"),
+							pedidoFilter.getDataPedidoInicio()));
+		}
+		
+		if(pedidoFilter.getDataPedidoFim() != null) {
+			predicates.add(criteriaBuilder
+					.lessThanOrEqualTo(root.get("dataEmissao"),
+							pedidoFilter.getDataPedidoFim()));
+		}
+		
+		if(pedidoFilter.getClienteId() != null) {
+			predicates.add(criteriaBuilder
+					.equal(root.get("cliente"), pedidoFilter.getClienteId()));
+		}
+		
+		if(pedidoFilter.getFuncionarioId() != null) {
+			predicates.add(criteriaBuilder
+					.equal(root.get("funcionario"), pedidoFilter.getFuncionarioId()));
+		}
+		
+		query.where(criteriaBuilder.and(predicates.toArray(new Predicate[0])));
+		TypedQuery<Pedido> queryResult = getEntityManager().createQuery(query);
+		return queryResult.getResultList();
+	}
 
 }
