f08f96d603c798fa9516b92da85d4a2ec55c0bf8
Fabio <fabio.s0@hotmail.com>
2023-09-23 16:53:02 -0300

    Update ClienteAssembler.java

--- Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/assembler/ClienteAssembler.java
+++ Primeiro-Crud/api/src/main/java/org/edu/unidep/api/dto/modelmapper/assembler/ClienteAssembler.java
@@ -5,9 +5,11 @@ import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.core.UriInfo;
 
 import org.edu.unidep.api.dto.modelmapper.request.ClienteRequest;
 import org.edu.unidep.api.dto.modelmapper.response.ClienteResponse;
+import org.edu.unidep.api.hypermedia.ApiLinks;
 import org.edu.unidep.domain.model.Cliente;
 import org.modelmapper.ModelMapper;
 
@@ -17,17 +19,26 @@ public class ClienteAssembler {
 	@Inject
 	private ModelMapper modelMapper;
 	
+	@Inject
+	private ApiLinks apiLinks;
+	
 	public Cliente toDomainObject(ClienteRequest clienteInput) {
 		return modelMapper.map(clienteInput, Cliente.class);
 	}
 	
-	public ClienteResponse toModel(Cliente cliente) {
-		return modelMapper.map(cliente, ClienteResponse.class);
+	public ClienteResponse toModel(Cliente cliente, UriInfo uriInfo) {
+		ClienteResponse clienteResponse = modelMapper.map(cliente, ClienteResponse.class);
+		clienteResponse.addLink(apiLinks.linkToClientesListar(uriInfo));
+		clienteResponse.addLink(apiLinks.linkToClientesBuscar(uriInfo, cliente.getId()));
+		clienteResponse.addLink(apiLinks.linkToClientesAtualizar(uriInfo, cliente.getId()));
+		clienteResponse.addLink(apiLinks.linkToClientesRegistrar(uriInfo));
+		clienteResponse.addLink(apiLinks.linkToClientesExcluir(uriInfo, cliente.getId()));
+		return clienteResponse;
 	}
 	
-	public List<ClienteResponse> toCollectionModel(List<Cliente> clientes){
+	public List<ClienteResponse> toCollectionModel(List<Cliente> clientes, UriInfo uriInfo){
 		return clientes.stream()
-				.map(cliente -> toModel(cliente))
+				.map(cliente -> toModel(cliente, uriInfo))
 				.collect(Collectors.toList());
 	}
 }
